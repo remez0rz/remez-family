487149bb2d3bc503359c021ca47522519507d77f
Raistlinator <ronen@tech.co.il>
2026-05-30 00:41:09 +0300

    Fix import paths in missions page

--- app/missions/page.js
+++ app/missions/page.js
@@ -1,8 +1,8 @@
 'use client'
 import { useEffect, useState } from 'react'
-import { supabase, getCurrentProfile } from '../../lib/supabase'
+import { supabase, getCurrentProfile } from '../lib/supabase'
 import { useRouter } from 'next/navigation'
-import BottomNav from '../../components/BottomNav'
+import BottomNav from '..components/BottomNav'
 
 const NAVY = '#0a1628'
 const GOLD = '#c9a84c'
